4a87a1e2b484df8e471d4c7075e9c3023e80f318
Oscar <cyberskely@gmail.com>
2023-09-18 23:26:30 +0100

    Create folder structure + install react

---
+++ frontend/src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="Player">1</div>
+      <div className="Chat">
+        <div className="SendMessage">
+          Test
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
