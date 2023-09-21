071a1f8b81d464c0876312e2bcc5c9328db3c803
Oscar <cyberskely@gmail.com>
2023-09-21 14:40:40 +0100

    Changed docker file structure

--- backend/index.ts
+++ backend/index.ts
@@ -6,10 +6,11 @@ app.get('/:userVal', (req, res) => {
   res.send({
     "Test": 7,
     "/x": req.params.userVal
-    
   });
 });
 
 app.listen(3000, () => {
   console.log("Started server");
 });
+
+console.log("Hello World");
